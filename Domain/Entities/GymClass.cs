bb8cd9f5088b3c4a65d207872b7018749d747bbf
Pablo Macieyko <macieyko.pablo@gmail.com>
2024-06-06 19:02:56 -0300

    Add project files.

---
+++ Domain/Entities/GymClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities; //preguntar que significa esto y porque algunos tienen llaves
+
+public class GymClass
+{
+    // va con decoradores?
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public float Price { get; set; }
+
+}
---
+++ Domain/Entities/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public abstract class User  //porque abstract?
+    {
+        //To do Alan Consultar
+
+        public int Id { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        public string FullName { get; set; }
+
+        [Required]  // porque?
+        [Column(TypeName = "nvarchar(100)")]
+        public string Email { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        public string Password { get; set; }
+        
+    }
+}
