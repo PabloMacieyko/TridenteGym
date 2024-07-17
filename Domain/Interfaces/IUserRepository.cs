4d925a099dc3c47c1e86e1efc484765a92f29af7
Pablo Macieyko <macieyko.pablo@gmail.com>
2024-07-17 20:58:26 -0300

    continuando

--- Domain/Interfaces/IUserRepository.cs
+++ Domain/Interfaces/IUserRepository.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace Domain.Interfaces
 {
-    internal interface IUserRepository
+    public interface IUserRepository
     {
+
     }
 }
