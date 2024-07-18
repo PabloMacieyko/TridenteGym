9d2978f800642d43604781de684a69085a2835a9
Pablo <123416062+PabloMacieyko@users.noreply.github.com>
2024-07-17 22:33:16 -0300

    Creadas entidad Activity, Owner y Professor (#4)

---
+++ TridenteGym.Api/Domain/Entities/Activity.cs
@@ -0,0 +1,30 @@
+﻿
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Entities
+{
+    public class Activity
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        [Column(TypeName = "nvarchar(100)")]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        [Column(TypeName = "nvarchar(500)")]
+        public string Description { get; set; } = string.Empty;
+
+        [Required]
+        public DateTime StartTime { get; set; }
+
+        [Required]
+        public DateTime EndTime { get; set; }
+
+        public Professor AssignedProfessor { get; set; }
+
+        public ICollection<Client> EnrolledClients { get; set; } = new List<Client>();
+    }
+}
---
+++ TridenteGym.Api/Domain/Entities/Professor.cs
@@ -0,0 +1,7 @@
+﻿namespace Domain.Entities
+{
+    public class Professor : User
+    {
+        public ICollection<Activity> AssignedActivities { get; set; } = new List<Activity>();
+    }
+}
