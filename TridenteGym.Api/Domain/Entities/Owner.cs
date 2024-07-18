dc709f682683d3cd9a4285992a6461063e01a207
Pablo Macieyko <macieyko.pablo@gmail.com>
2024-07-17 22:32:55 -0300

    Creadas entidad Activity, Owner y Professor

---
+++ TridenteGym.Api/Domain/Entities/Owner.cs
@@ -0,0 +1,10 @@
+﻿
+
+namespace Domain.Entities
+{
+    public class Owner : User
+    {
+        public ICollection<Client> Clients { get; set; } = new List<Client>();
+        public ICollection<Professor> Professors { get; set; } = new List<Professor>();
+    }
+}
