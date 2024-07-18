6276c9e97b40fcad4db37a25733b8a7db1d95c41
Pablo <123416062+PabloMacieyko@users.noreply.github.com>
2024-07-17 22:25:43 -0300

    Creadas entidad User y Client (#3)

---
+++ TridenteGym.Api/Domain/Entities/Client.cs
@@ -0,0 +1,10 @@
+﻿
+using System.Diagnostics;
+
+namespace Domain.Entities
+{
+    public class Client : User
+    {
+        public ICollection<Activity> EnrolledActivities { get; set; } = new List<Activity>();
+    }
+}
---
+++ TridenteGym.Api/Domain/Entities/User.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Entities
+{
+    public abstract class User
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        [Column(TypeName = "nvarchar(20)")]
+        public string UserType { get; set; } = string.Empty;
+
+        [Required]
+        [Column(TypeName = "nvarchar(100)")]
+        public string FullName { get; set; } = string.Empty;
+
+        [Required]
+        [Column(TypeName = "nvarchar(100)")]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        [Column(TypeName = "nvarchar(100)")]
+        public string Password { get; set; } = string.Empty;
+    }
+}
