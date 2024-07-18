4fbabf0d1ce88384efcb17c566cdfe038f1660f4
Hector Mendoza <hectormaximendoza@gmail.com>
2024-07-17 22:52:22 -0300

    Creadas Intterfaces de capa Domain.

---
+++ TridenteGym.Api/Domain/Interfaces/IActivityRepository.cs
@@ -0,0 +1,12 @@
+﻿
+using Domain.Entities;
+
+namespace Domain.Interfaces
+{
+    public interface IActivityRepository : IRepositoryBase<Activity>
+    {
+        Task<List<Client>> GetEnrolledClientsAsync(int activityId, CancellationToken cancellationToken = default);
+        Task<Professor> GetAssignedProfessorAsync(int activityId, CancellationToken cancellationToken = default);
+
+    }
+}
---
+++ TridenteGym.Api/Domain/Interfaces/IProfessorRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Entities;
+
+namespace Domain.Interfaces
+{
+    public interface IProfessorRepository : IRepositoryBase<Professor>
+    {
+        Task<List<Activity>> GetAssignedActivitiesAsync(int professorId, CancellationToken cancellationToken = default);
+    }
+}
