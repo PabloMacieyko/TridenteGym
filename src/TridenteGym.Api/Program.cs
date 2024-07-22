1f4db52bb6c893035bfe5964e4e416406dcc570b
Pablo Macieyko <macieyko.pablo@gmail.com>
2024-07-22 14:18:38 -0300

    Add program and appsetting

--- src/TridenteGym.Api/Program.cs
+++ src/TridenteGym.Api/Program.cs
@@ -1,3 +1,16 @@
+using Application.Interfaces;
+using Application.Services;
+using Domain.Interfaces;
+using Infrastructure.Data;
+using Infrastructure.Repositories;
+using Infrastructure.Services;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
+using static Infrastructure.Services.AuthenticationService;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,7 +18,73 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(setupAction =>
+{
+    setupAction.AddSecurityDefinition("TridenteGym", new OpenApiSecurityScheme()
+    {
+        Type = SecuritySchemeType.Http,
+        Scheme = "Bearer",
+        Description = "Token generado"
+    });
+
+    setupAction.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "TridenteGym"
+                }
+            },
+            new List<string>()
+        }
+    });
+});
+
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
+builder.Configuration["ConnectionStrings:DefaultConnection"], b => b.MigrationsAssembly("Infrastructure")));
+
+builder.Services
+    .AddAuthentication("Bearer") //"Bearer" es el tipo de auntenticación que tenemos que elegir después en PostMan para pasarle el token
+    .AddJwtBearer(options => //Acá definimos la configuración de la autenticación. le decimos qué cosas queremos comprobar. La fecha de expiración se valida por defecto.
+    {
+        options.TokenValidationParameters = new()
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["AuthenticacionService:Issuer"],
+            ValidAudience = builder.Configuration["AuthenticacionService:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(builder.Configuration["AuthenticacionService:SecretForKey"]))
+        };
+    }
+);
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("OwnerPolicy", policy => policy.RequireRole("Owner"));
+    options.AddPolicy("ProfessorPolicy", policy => policy.RequireRole("Professor"));
+    options.AddPolicy("ClientPolicy", policy => policy.RequireRole("Client"));
+});
+
+#region Repositories and Services
+// Dependency Injection of Services and Repositories
+builder.Services.AddScoped<IClientService, ClientService>();
+builder.Services.AddScoped<IClientRepository, ClientRepository>();
+builder.Services.AddScoped<IProfessorService, ProfessorService>();
+builder.Services.AddScoped<IProfessorRepository, ProfessorRepository>();
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IActivityService, ActivityService>();
+builder.Services.AddScoped<IActivityRepository, ActivityRepository>();
+builder.Services.AddScoped<IEnrollmentService, EnrollmentService>();
+builder.Services.AddScoped<IEnrollmentRepository, EnrollmentRepository>();
+builder.Services.Configure<AuthenticacionServiceOptions>(
+    builder.Configuration.GetSection(AuthenticacionServiceOptions.AuthenticacionService));
+builder.Services.AddScoped<ICustomAuthentication, AuthenticationService>();
+#endregion
 
 var app = builder.Build();
 
@@ -17,6 +96,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
