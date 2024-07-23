3e9e563066b56f7e9da38f578c799af8d8e17cf8
hector_mendoza_711 <141861292+hmendoza711@users.noreply.github.com>
2024-07-22 23:25:23 -0300

    Creacion de Repositories, ApplicationDbContext, AuthenticationService en Capa Infrastructure. (#10)
    Creacion de Controllers e Instalacion de paquete NuGet JwtBearer en capa Api.
    
    Co-authored-by: Hector Mendoza <hectormaximendoza@gmail.com>

---
+++ src/Infrastructure/Repositories/EnrollmentRepository.cs
@@ -0,0 +1,14 @@
+﻿
+using Domain.Entities;
+using Domain.Interfaces;
+using Infrastructure.Data;
+
+namespace Infrastructure.Repositories
+{
+    public class EnrollmentRepository : BaseRepository<Enrollment>, IEnrollmentRepository
+    {
+        public EnrollmentRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+    }
+}
---
+++ src/Infrastructure/Repositories/ProfessorRepository.cs
@@ -0,0 +1,30 @@
+﻿using Domain.Entities;
+using Domain.Interfaces;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories
+{
+    public class ProfessorRepository : EfRepository<Professor>, IProfessorRepository
+    {
+        public ProfessorRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+
+        public ICollection<Client> GetClientsEnrolledInMyActivities(int professorId)
+        {
+            var professor = _appDbContext.Professors
+                .Include(p => p.Activities)
+                .ThenInclude(a => a.Enrollments)
+                .ThenInclude(e => e.Client)
+                .SingleOrDefault(p => p.Id == professorId);
+
+            if (professor == null)
+            {
+                throw new KeyNotFoundException($"Professor with ID {professorId} not found.");
+            }
+
+            return professor.Activities.SelectMany(a => a.Enrollments.Select(e => e.Client)).ToList();
+        }
+    }
+}
