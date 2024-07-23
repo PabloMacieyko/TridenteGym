808c8539915018762869c2517e954863e54da5d8
Hector Mendoza <hectormaximendoza@gmail.com>
2024-07-22 21:21:00 -0300

    Create ProfessorRepository

---
+++ src/Infrastructure/Repositories/ProfessorRepository.cs
@@ -0,0 +1,67 @@
+﻿
+using Domain.Entities;
+using Domain.Interfaces;
+using Infrastructure.Data;
+
+namespace Infrastructure.Repositories
+{
+    public class ProfessorRepository : IProfessorRepository
+    {
+        private readonly ApplicationDbContext _appDbContext;
+
+        public ProfessorRepository(ApplicationDbContext appdbcontex)
+        {
+            _appDbContext = appdbcontex;
+        }
+
+        public Professor Create(Professor professor)
+        {
+            var result = _appDbContext.Professors.Add(professor);
+            _appDbContext.SaveChanges();
+
+            return result.Entity;
+        }
+
+        public void Delete(int id)
+        {
+            var professor = _appDbContext.Professors.FirstOrDefault(x => x.Id == id);
+            if (professor == null)
+                throw new Exception("Profesor no encontrado");
+
+            _appDbContext.Professors.Remove(professor);
+            _appDbContext.SaveChanges();
+        }
+
+        public Professor Get(int id)
+        {
+            return _appDbContext.Professors.FirstOrDefault(x => x.Id == id) ?? throw new Exception("Profesor no encontrado");
+        }
+
+        public IList<Professor> GetAll()
+        {
+            return _appDbContext.Professors.ToList();
+        }
+
+        public ICollection<Client> GetClientsEnrolledInMyActivities(int professorId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Professor Update(Professor professor, int id)
+        {
+            var result = _appDbContext.Professors.FirstOrDefault(x => x.Id == id);
+            if (result == null)
+                throw new Exception("Profesor no encontrado");
+            else
+            {
+                result.Name = professor.Name;
+                result.Email = professor.Email;
+                result.Password = professor.Password;
+            }
+            _appDbContext.Professors.Update(professor);
+            _appDbContext.SaveChanges();
+
+            return result;
+        }
+    }
+}
