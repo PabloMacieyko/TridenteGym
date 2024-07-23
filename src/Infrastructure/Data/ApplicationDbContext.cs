570268707edb9c72866ac0f024dd390a966db107
Hector Mendoza <hectormaximendoza@gmail.com>
2024-07-22 23:23:13 -0300

    Creacion de Repositories, ApplicationDbContext, AuthenticationService en Capa Infrastructure. Creacion de Controllers e Instalacion de paquete NuGet JwtBearer en capa Api.

---
+++ src/Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,64 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using System.Reflection;
+
+namespace Infrastructure.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        public DbSet<User> Users => Set<User>();
+        public DbSet<Client> Clients => Set<Client>();
+        public DbSet<Professor> Professors => Set<Professor>();
+        public DbSet<Owner> Owners { get; set; }
+        public DbSet<Enrollment> Enrollments => Set<Enrollment>();
+        public DbSet<Activity> Activities => Set<Activity>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            // Configura la herencia TPH (Table Per Hierarchy) para la entidad User y sus derivados
+            modelBuilder.Entity<User>()
+                .HasDiscriminator<string>("Discriminator")
+                .HasValue<User>("User")
+                .HasValue<Client>("Client")
+                .HasValue<Professor>("Professor")
+                .HasValue<Owner>("Owner");
+            // Configuración de la conversión de enums
+            modelBuilder.Entity<User>()
+                    .Property(d => d.Role)
+                    .HasConversion(
+                        v => v.ToString(),
+                        v => (UserRole)Enum.Parse(typeof(UserRole), v));
+            // Configuración de las relaciones entre entidades
+            modelBuilder.Entity<Enrollment>()
+                .HasKey(e => e.EnrollmentId);
+
+            modelBuilder.Entity<Enrollment>()
+                .HasOne(e => e.Client)
+                .WithMany(c => c.Enrollments)
+                .HasForeignKey(e => e.ClientId)
+                .OnDelete(DeleteBehavior.Restrict); // Evitar eliminación en cascada
+
+            modelBuilder.Entity<Enrollment>()
+                .HasOne(e => e.Activity)
+                .WithMany(a => a.Enrollments)
+                .HasForeignKey(e => e.ActivityId)
+                .OnDelete(DeleteBehavior.Restrict); // Evitar eliminación en cascada
+
+            // Configuración de la relación entre Activity y Professor
+            modelBuilder.Entity<Activity>()
+                .HasOne(a => a.Professor)
+                .WithMany(p => p.Activities)
+                .HasForeignKey(a => a.ProfessorId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
---
+++ src/Infrastructure/Repositories/ActivityRepository.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Entities;
+using Domain.Interfaces;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories
+{
+    public class ActivityRepository : EfRepository<Activity>, IActivityRepository
+    {
+        public ActivityRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+        public async Task<List<Activity>> GetActivitiesByProfessorIdAsync(int professorId, CancellationToken cancellationToken = default)
+        {
+            return await _appDbContext.Activities
+                .Where(activity => activity.ProfessorId == professorId)
+                .ToListAsync(cancellationToken);
+        }
+    }
+}
